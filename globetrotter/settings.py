3edcd65ddcd04a118aad5d80e672d107e01df4f7
kuhanimkuu <davidmurunga@gmail.com>
2025-09-28 11:25:22 +0300

    update: updated settings.py to fix error during deployment on render.com

--- globetrotter/settings.py
+++ globetrotter/settings.py
@@ -91,13 +91,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:4173",
-    'https://globe-trotter-frontend.vercel.app/',
+    'https://globe-trotter-frontend.vercel.app',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    'https://globe-trotter-frontend.vercel.app/',
+    'https://globe-trotter-frontend.vercel.app',
 ]
 TEMPLATES = [
     {
